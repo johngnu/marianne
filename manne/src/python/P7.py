bf90169e8486fe3010526bdb6d8ac464bc07ad3c
John Castillo <john.gnu@gmail.com>
2025-01-14 14:36:58 -0400

    2

--- manne/src/python/P7.py
+++ manne/src/python/P7.py
@@ -1,7 +1,22 @@
-#inicio
+# Ejercicio 11
+# inicio
 
-n=int(input("ingresa un numero: "))
-
-print(n)
-
-#fin
+x = int(input("Ingresa n: "))
+for j in range(1, x + 1):
+    n = int(input())
+    c = 0
+    k = 1
+    t = 1
+    for i in range(1, n + 1):
+        print(t, ",", end="")
+        if i == 1:
+            t = t + k
+            k = k + 1
+        else:
+            t = t + k
+            c = c + 1
+            if c == 2:
+                k = k + 1;
+                c = 0
+    print()
+# fin
