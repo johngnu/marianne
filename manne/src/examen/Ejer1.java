5081164b21a0ecd683820ef703956dc24b9ee369
John Castillo <john.gnu@gmail.com>
2025-01-14 14:19:49 -0400

    uno

--- manne/src/examen/Ejer1.java
+++ manne/src/examen/Ejer1.java
@@ -12,12 +12,43 @@ public class Ejer1 {
     /* Ejercicio 1 */
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Ingresa n: ");
-        int n = teclado.nextInt();
-        
-        
-        // hola
+        System.out.println("Ingresar numeros: ");
+        int a = teclado.nextInt();
+        int b = teclado.nextInt();
+        int c = teclado.nextInt();
+        int d = teclado.nextInt();
 
-        System.out.println("Ingresaste: " + n);
+        int x = a + b;
+        int y = c + d;
+        if (x > y) {
+            System.out.println("Anidados");
+            if (a > b && a > c && a > d) {
+                System.out.println(a + " es mayor");
+            } else {
+                if (b > a && b > c && b > d) {
+                    System.out.println(b + " es mayor");
+                } else {
+                    if (c > a && c > b && c > d) {
+                        System.out.println(c + " es mayor");
+                    } else {
+                        System.out.println(d + " es mayor");
+                    }
+                }
+            }
+        } else {
+            System.out.println("No Anidados");
+            int min = a;
+            if (b < min) {
+                min = b;
+            }
+            if (c < min) {
+                min = c;
+            }
+            if (d < min) {
+                min = d;
+            }
+            System.out.println(min + " es el minimo");
+        }
+
     }
 }
